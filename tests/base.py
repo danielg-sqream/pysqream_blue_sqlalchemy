56fda5e55b266b29ba035ef38f2d45f931a9035b
danielg-sqream <100589834+danielg-sqream@users.noreply.github.com>
2023-04-18 13:00:31 +0300

    Danielg auto zero protocol 21022023 (#1)
    SqlAlchemy support Auth0

--- tests/base.py
+++ tests/base.py
@@ -6,11 +6,14 @@ from pytest_logger import Logger
 from sqlalchemy import orm, create_engine, MetaData
 import sqlalchemy as sa
 
+_access_token="Z2hZaDdyMmhEWHFkdGJBN3c4em9SSndjcVBXQjI5a05XZjRHSHU4X1B0R1RmbzYzYm53NENGaUVIMGlSX0lLRjVhMUQ3c3JfbHQyVGtfRk1md3U5T1M3aXNlZlcwS2l4"
 
-def connect(ip, clustered=False, use_ssl=False):
+
+def connect(domain, clustered=False, use_ssl=False):
     print_echo = False
-    conn_str = f"pysqream+dialect://sqream:sqream@{ip}:5000/master"
-    engine = create_engine(conn_str, echo=print_echo)
+    conn_str = f"sqream_blue://{domain}:443/master"
+    connect_args = {'access_token': _access_token}
+    engine = create_engine(conn_str, connect_args=connect_args, echo=print_echo)
     sa.Tinyint = engine.dialect.Tinyint
     session = orm.sessionmaker(bind=engine)()
     metadata = MetaData()
@@ -25,27 +28,15 @@ def setTinyint(engine):
 class TestBase():
 
     @pytest.fixture()
-    def ip(self, pytestconfig):
-        return pytestconfig.getoption("ip")
+    def domain(self, pytestconfig):
+        return pytestconfig.getoption("domain")
 
     @pytest.fixture(autouse=True)
-    def Test_setup_teardown(self, ip):
-        ip = ip if ip else socket.gethostbyname(socket.gethostname())
+    def Test_setup_teardown(self, domain):
         Logger().info("Before Scenario")
-        Logger().info(f"Connect to server {ip}")
-        self.ip = ip
-        self.engine ,self.metadata ,self.session, self.conn_str = connect(ip)
+        Logger().info(f"Connect to server {domain}")
+        self.domain = domain
+        self.engine ,self.metadata ,self.session, self.conn_str = connect(domain)
         setTinyint(self.engine)
         yield
         Logger().info("After Scenario")
-
-
-class TestBaseWithoutBeforeAfter():
-    @pytest.fixture()
-    def ip(self, pytestconfig):
-        return pytestconfig.getoption("ip")
-
-    @pytest.fixture(autouse=True)
-    def Test_setup_teardown(self, ip):
-        self.ip = ip if ip else socket.gethostbyname(socket.gethostname())
-        yield
