ed110af697c7e1218a3277f897076f3a65d28bed
zachik <zachik@sqream.com>
2024-09-22 11:39:35 +0300

    refactor after review

--- tests/base.py
+++ tests/base.py
@@ -6,13 +6,12 @@ from pytest_logger import Logger
 from sqlalchemy import orm, create_engine, MetaData
 import sqlalchemy as sa
 
-_access_token="Z2hZaDdyMmhEWHFkdGJBN3c4em9SSndjcVBXQjI5a05XZjRHSHU4X1B0R1RmbzYzYm53NENGaUVIMGlSX0lLRjVhMUQ3c3JfbHQyVGtfRk1md3U5T1M3aXNlZlcwS2l4"
+_access_token="Zk5QN1RxUXNzVTZPTmJiN3I2a2duekNFTXAwYVhKUVlISTltYVVuTllUdnhkb3lZRUxJeDJOT3pXZU5sM0Jzb09uVkNsSl9USEJPc1RFQlVISXowRDdTZ1dkaEM1UDBt"
 
-
-def connect(domain, clustered=False, use_ssl=False):
+def connect(domain, clustered=False, use_ssl=False, access_token=_access_token):
     print_echo = False
     conn_str = f"sqream_blue://{domain}:443/master"
-    connect_args = {'access_token': _access_token}
+    connect_args = {'access_token': access_token}
     engine = create_engine(conn_str, connect_args=connect_args, echo=print_echo)
     sa.Tinyint = engine.dialect.Tinyint
     session = orm.sessionmaker(bind=engine)()
@@ -31,12 +30,18 @@ class TestBase():
     def domain(self, pytestconfig):
         return pytestconfig.getoption("domain")
 
+    @pytest.fixture()
+    def access_token(self, pytestconfig):
+        return pytestconfig.getoption("access_token")
+
+
     @pytest.fixture(autouse=True)
-    def Test_setup_teardown(self, domain):
+    def Test_setup_teardown(self, domain, access_token):
         Logger().info("Before Scenario")
         Logger().info(f"Connect to server {domain}")
         self.domain = domain
-        self.engine ,self.metadata ,self.session, self.conn_str = connect(domain)
+        self.engine ,self.metadata ,self.session, self.conn_str = connect(domain, access_token=access_token)
         setTinyint(self.engine)
         yield
         Logger().info("After Scenario")
+        self.engine.dispose()
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@
 
 def pytest_addoption(parser):
     parser.addoption("--domain", action="store", help="SQream Server domain")
+    parser.addoption("--access_token", action="store", help="SQream server token")
 
 
 def pytest_generate_tests(metafunc):
     metafunc.config.getoption("domain")
+    metafunc.config.getoption("access_token")
